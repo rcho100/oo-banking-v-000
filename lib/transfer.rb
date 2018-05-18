524a2f326d3e2d6ad724f1aaef6a37abfb9c9c12
Richard Cho <richardcho100@gmail.com>
2018-05-18 21:22:15 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,15 +19,17 @@ class Transfer
   end
   def execute_transaction
     counter = 0
-    if self.sender.valid? && self.sender.balance >= self.amount
+    if self.sender.valid? && self.sender.balance >= self.amount && counter = 0
       counter += 1
       self.sender.balance -= self.amount
       self.receiver.balance += self.amount
       self.status = "complete"
     else
-      counter += 1
-      self.status = "rejected"
-      "Transaction rejected. Please check your account balance."
+      if counter = 0
+        endcounter += 1
+        self.status = "rejected"
+        "Transaction rejected. Please check your account balance."
+      end
     end
   end
 
