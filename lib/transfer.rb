7f6720609e89af1574c27b6136a258ec785b41d3
Richard Cho <richardcho100@gmail.com>
2018-05-18 21:40:31 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -26,7 +26,7 @@ class Transfer
   end
 
   def reverse_transfer
-    self.valid?
+    if valid? && self.status == "complete"
     self.receiver.balance -= self.amount
     self.sender.balance += self.amount
     self.status = "reversed"
